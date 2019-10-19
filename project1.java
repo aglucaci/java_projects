1897af2a79da7335241dc45a6b9b15389c3d8a0e
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-10-19 11:21:29 -0400

    Create project1.java

---
+++ project1.java
@@ -0,0 +1,5 @@
+The point of this program is to read in a fasta file and give nucleotide frequency.
+
+
+Additional: I want it to be able to read in .FNA format (Fasta with Newick tree). 
+-Have the program read in the fasta and load it into a list. Also store the newick as a string.
